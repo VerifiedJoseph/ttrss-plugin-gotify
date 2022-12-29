924ac6910b3c0c9edaf693ece6231fa452091fb7
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-12-29 01:33:51 +0000

    Update verifiedjoseph/gotify-api-php (v1.7.0 => v1.7.2)

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,25 +33,18 @@ class ComposerAutoloaderInitc8907ddafab92b0ee3e3ad9d13ad042a
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInitc8907ddafab92b0ee3e3ad9d13ad042a::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequirec8907ddafab92b0ee3e3ad9d13ad042a($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitc8907ddafab92b0ee3e3ad9d13ad042a::$files;
+        $requireFile = static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        };
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            ($requireFile)($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequirec8907ddafab92b0ee3e3ad9d13ad042a($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
