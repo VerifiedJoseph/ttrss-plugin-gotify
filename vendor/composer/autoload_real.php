7ed7d97f0ecd1cc0088c8e18261534800260107e
Joseph <VerifiedJoseph@users.noreply.github.com>
2023-02-17 22:41:30 +0000

    Update verifiedjoseph/gotify-api-php (v1.7.3 => v1.7.4)

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -34,15 +34,15 @@ class ComposerAutoloaderInitc8907ddafab92b0ee3e3ad9d13ad042a
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInitc8907ddafab92b0ee3e3ad9d13ad042a::$files;
-        $requireFile = static function ($fileIdentifier, $file) {
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
             if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
                 $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
 
                 require $file;
             }
-        };
+        }, null, null);
         foreach ($filesToLoad as $fileIdentifier => $file) {
-            ($requireFile)($fileIdentifier, $file);
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
